64aa636a4b34fa31994e743e7e80b8c751784d2a
Maksim <pelmesh10@gmail.com>
2018-09-03 10:56:17 +0300

    sign-in fixes

--- src/pages/sign-in.vue
+++ src/pages/sign-in.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="d-flex align-items-center vh-100 container-fluid">
         <div class="mx-auto w-100 sign-form current-shadow bg-white rounded mb-3">
-            <div class="text-center bg-theme rounded-top px-4 py-3">
+            <div class="text-center bg-theme rounded-top px-4 py-1">
                 <img src="@/assets/logo.png"
                      width="150"
                      class="my-2"
                      alt="logo">
             </div>
             <form v-on:submit.prevent="signIn"
-                  class="px-5 pt-4 pb-3">
+                  class="px-5 pt-4 pb-2">
                 <div class="form-group">
                     <input type="email"
                            name="email"
@@ -37,11 +37,14 @@
                 </div>
                 <span class="text-danger d-block text-center">{{errorMessage}}</span>
             </form>
+            <a href="#"
+               class="text-center d-block mb-4 text-muted"
+               @click.prevent>Восстановить пароль</a>
         </div>
     </div>
 </template>
 <script>
-import { AdminApi } from '@/services/api'
+import { AdminApi } from '@/services/api';
 export default {
     name: 'SignIn',
     data() {
@@ -50,22 +53,25 @@ export default {
             pass: '',
             errorMessage: '',
             showPassword: false
-        }
+        };
     },
     methods: {
         signIn() {
             let options = {
                 email: this.email,
                 password: this.pass
-            }
-            this.$http.post(AdminApi.signIn, options).then(() => {
-                this.$router.push({ name: 'Panel' })
-            }).catch((err) => {
-                this.errorMessage = err.body.messages[0]
-            })
-        },
+            };
+            this.$http
+                .post(AdminApi.signIn, options)
+                .then(() => {
+                    this.$router.push({ name: 'Panel' });
+                })
+                .catch(err => {
+                    this.errorMessage = 'Неверные данные';
+                });
+        }
     }
-}
+};
 </script>
 <style scoped>
 </style>
